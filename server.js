37b0bf9ebc6178c153014fe37f15433835be68bf
Jennifer Henry <jenhenry1995@gmail.com>
2020-11-07 22:08:19 -0800

    meh

--- server.js
+++ server.js
@@ -12,12 +12,16 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/dbExercise", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-});
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/dbExercise',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+  );
+  
 
 // routes
 app.use(require("./routes/apiRoutes.js"));
